821d6e4c3a4e582ab7c1062aaaa71d91467ce537
gso-fe <110996927+gso-fe@users.noreply.github.com>
2023-03-15 21:18:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const filmDescriptions = [];
-const filmCharacters = [];
 const filmsForm = document.querySelector('#filmsForm');
 const teaserText = document.querySelector('#teaserText');
 teaserText.innerHTML = "Kein Film ausgewählt ...";
@@ -10,17 +9,10 @@ fetch('https://swapi.dev/api/films/')
     const films = data.results;
     films.forEach(film => {
       const option = document.createElement("option");
-      const filmCharacterItem = {};
       option.value = film.url;
       option.text = film.title;
       filmsForm.appendChild(option);
       filmDescriptions.push(film.opening_crawl);
-      filmCharacterItem.title = film.title;
-      filmCharacterItem.characters = film.characters;
-      filmCharacters.push(filmCharacterItem);
-      return film.characters.forEach(item => {
-        fetch(item);
-      });
     })
   })
   .catch(error => console.error(error));
